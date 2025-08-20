179f85ce51ce4a9c3c3d1da91f0ab107db58ac95
unknown <vova.vevo666@gmail.com>
2025-08-21 00:54:54 +0300

    add pause manager

---
+++ Assets/Scripts/PauseManager.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class PauseManager : MonoBehaviour
+    {
+        [SerializeField] private GameObject pauseUI;
+
+        private bool userPaused = false;    // пауза через кнопку
+        private bool systemPaused = false;  // пауза из-за потери фокуса
+
+        private void Start()
+        {
+            Resume();
+        }
+
+        private void OnApplicationFocus(bool hasFocus)
+        {
+            systemPaused = !hasFocus;
+            UpdatePause();
+        }
+
+        private void OnApplicationPause(bool pauseStatus)
+        {
+            systemPaused = pauseStatus;
+            UpdatePause();
+        }
+
+        private void UpdatePause()
+        {
+            bool shouldPause = userPaused || systemPaused;
+
+            Time.timeScale = shouldPause ? 0f : 1f;
+            AudioListener.pause = shouldPause;
+
+            if (pauseUI != null)
+                pauseUI.SetActive(userPaused); // показываем UI только при пользовательской паузе
+        }
+
+        // Вызывается кнопкой
+        public void Pause()
+        {
+            userPaused = true;
+            UpdatePause();
+        }
+
+        // Вызывается кнопкой
+        public void Resume()
+        {
+            userPaused = false;
+            UpdatePause();
+        }
+    }
+}
---
+++ Assets/Scripts/UI/UIController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts.UI
+{
+    public class UIController : MonoBehaviour
+    {
+        [SerializeField] private PauseManager pauseManager;
+        public void PauseButton()
+        {
+            pauseManager.Pause();
+        }
+
+        public void ResumeButton()
+        {
+            pauseManager.Resume();
+        }
+    }
+}
