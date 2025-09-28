fe6ad4b979bb87b04a896d44538c2bb5c543679c
unknown <vova.vevo666@gmail.com>
2025-09-29 00:58:28 +0300

    ui money

--- Assets/Scripts/UI/UIController.cs
+++ Assets/Scripts/UI/UIController.cs
@@ -1,15 +1,24 @@
 ﻿using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 namespace Assets.Scripts.UI
 {
     public class UIController : MonoBehaviour
     {
         [SerializeField] private PauseManager pauseManager;
+        [SerializeField] private ScoreManager scoreManager;
 
         [SerializeField] private GameObject winPanel;
         [SerializeField] private GameObject losePanel;
+
+        [SerializeField] private TextMeshProUGUI textMoney;
+
+        private void Update()
+        {
+            textMoney.text = scoreManager.GetCurrentMoney().ToString();
+        }
 
         public void PauseButton()
         {
