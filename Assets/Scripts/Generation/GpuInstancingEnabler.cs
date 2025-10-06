3e35c2bad353c6286327256a633bfe0c0c4acc6e
unknown <vova.vevo666@gmail.com>
2025-10-07 01:25:45 +0300

    optimization

---
+++ Assets/Scripts/Generation/GpuInstancingEnabler.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts.Generation
+{
+    [RequireComponent(typeof(MeshRenderer))]
+    public class GpuInstancingEnabler : MonoBehaviour
+    {
+        private void Awake()
+        {
+            MaterialPropertyBlock materialPropertyBlock = new MaterialPropertyBlock();
+            MeshRenderer meshRenderer = GetComponent<MeshRenderer>();
+            meshRenderer.SetPropertyBlock(materialPropertyBlock);
+        }
+    }
+}
